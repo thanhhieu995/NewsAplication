625d35f8738f8f78d727446aa89dd2ccfddf1118
Hieu <thanhhieu995@gmail.com>
2022-01-20 16:21:53 +0700

    Create AdapterSearch and layout_item_search.

--- app/src/main/java/com/example/newsapplication/search/SearchActivity.java
+++ app/src/main/java/com/example/newsapplication/search/SearchActivity.java
@@ -1,6 +1,9 @@
 package com.example.newsapplication.search;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
 
@@ -8,9 +11,17 @@ import com.example.newsapplication.R;
 
 public class SearchActivity extends AppCompatActivity {
 
+    SearchView searchView;
+    RecyclerView rvSearch;
+    SwipeRefreshLayout refreshLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        searchView = findViewById(R.id.searchView);
+        rvSearch = findViewById(R.id.rv_Search);
+        refreshLayout = findViewById(R.id.swipeRefresh_Search);
     }
 }
