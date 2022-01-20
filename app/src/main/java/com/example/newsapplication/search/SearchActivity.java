7900bacde1ca92684bc9c598126ae3e7304ad191
Hieu <thanhhieu995@gmail.com>
2022-01-20 23:49:29 +0700

    Click searchView enter keyword, callAPI(keyword,...).

--- app/src/main/java/com/example/newsapplication/search/SearchActivity.java
+++ app/src/main/java/com/example/newsapplication/search/SearchActivity.java
@@ -1,19 +1,34 @@
 package com.example.newsapplication.search;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.SearchView;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
+import android.widget.SearchView;
 
 import com.example.newsapplication.R;
+import com.example.newsapplication.api.RetrofitAPI;
+import com.example.newsapplication.api.RetrofitClient;
+import com.example.newsapplication.main.NewsRVAdapter;
+import com.example.newsapplication.model.Articles;
+import com.example.newsapplication.model.NewsModal;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class SearchActivity extends AppCompatActivity {
 
     SearchView searchView;
     RecyclerView rvSearch;
     SwipeRefreshLayout refreshLayout;
+
+    ArrayList<Articles> articlesArrayList;
+    NewsRVAdapter newsRVAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +38,48 @@ public class SearchActivity extends AppCompatActivity {
         searchView = findViewById(R.id.searchView);
         rvSearch = findViewById(R.id.rv_Search);
         refreshLayout = findViewById(R.id.swipeRefresh_Search);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        rvSearch.setLayoutManager(layoutManager);
+        newsRVAdapter = new NewsRVAdapter(articlesArrayList, this);
+        rvSearch.setAdapter(newsRVAdapter);
+
+        searchView.setIconified(false);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                callApi(query, "3dde52248f66463eb8ef34f3d19cb936");
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+    }
+
+    private void callApi(String keyWord, String apiKey) {
+        //articlesArrayList.clear();
+        RetrofitAPI service = RetrofitClient.getClient().create(RetrofitAPI.class);
+        Call<NewsModal> call = service.getSearchNews(keyWord, apiKey);
+        call.enqueue(new Callback<NewsModal>() {
+            @Override
+            public void onResponse(Call<NewsModal> call, Response<NewsModal> response) {
+                if (response.body() != null) {
+                     NewsModal newsModal = response.body();
+                     ArrayList<Articles> articles = newsModal.getArticles();
+                     for (int i = 0; i < articles.size(); i++) {
+                         articlesArrayList.add(new Articles(articles.get(i).getTitle(), articles.get(i).getDescription(), articles.get(i).getUrlToImage(), articles.get(i).getUrl(), articles.get(i).getContent()));
+                     }
+                     newsRVAdapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<NewsModal> call, Throwable t) {
+                t.printStackTrace();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/newsapplication/search/SearchAdapter.java
+++ app/src/main/java/com/example/newsapplication/search/SearchAdapter.java
@@ -1,16 +1,23 @@
 package com.example.newsapplication.search;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.newsapplication.R;
 
 public class SearchAdapter extends RecyclerView.Adapter<SearchAdapter.ViewHolder> {
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_search_item, parent, false);
+        ViewHolder viewHolder = new ViewHolder(view);
+        return viewHolder;
     }
 
     @Override
@@ -24,8 +31,16 @@ public class SearchAdapter extends RecyclerView.Adapter<SearchAdapter.ViewHolder
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView imageView;
+        TextView txtTitle;
+        TextView txtContent;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            imageView = itemView.findViewById(R.id.imgLayout_Search);
+            txtTitle = itemView.findViewById(R.id.item_Search_txtTitleNews);
+            txtContent = itemView.findViewById(R.id.item_Search_txtContent);
         }
     }
 }
