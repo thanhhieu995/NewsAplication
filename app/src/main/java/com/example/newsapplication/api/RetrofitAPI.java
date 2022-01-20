0f31ab8b12d24207c7fde4b829705a07dc0ec79a
Hieu <thanhhieu995@gmail.com>
2022-01-20 15:52:14 +0700

    Add getSearchNews(..) to RetrofitAPI

--- app/src/main/java/com/example/newsapplication/api/RetrofitAPI.java
+++ app/src/main/java/com/example/newsapplication/api/RetrofitAPI.java
@@ -14,4 +14,7 @@ public interface RetrofitAPI {
 
     @GET("v2/top-headlines")
     Call<NewsModal> getNewsByCategory(@Query("country") String country, @Query("category") String category, @Query("apiKey") String apiKey);
+
+    @GET("v2/everything")
+    Call<NewsModal> getSearchNews(@Query("q") String keyWord, @Query("apiKey") String apiKey);
 }
